8f3928d346c8210873e87e477af9881278329d82
Soureesh Varma <soureeshvarma2004@gmail.com>
2025-04-15 13:07:23 +0530

    Fix Contact page visibility and button, align elements, add responsive zooming, resolve 404 on refresh

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; // Optional global styles
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import ErrorBoundary from './components/ErrorBoundary';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <BrowserRouter basename="/MyPortfolio">
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </BrowserRouter>
   </React.StrictMode>
 );
